33fdc70d94ceee927dce0bd59d31bdc948258153
Shahziyaa <shahziya2003@gmail.com>
2025-09-19 10:36:34 +0530

    Add initial SQL files for students and homework data

---
+++ classwork/class1.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO students (id,name, age,department,grade) 
+VALUES ('1','Alfred','21','computer science','67'),
+('2','Kiran','12','maths','98'),
+('3','Chong','23','physics','90'),
+('4','Suresh','32','computer science','93');
