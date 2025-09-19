0a717dfc06f6100441040350d8e8d1cb1bb9ee27
Shahziyaa <shahziya2003@gmail.com>
2025-09-19 10:52:28 +0530

    Add query to select students older than 20

--- classwork/class1.sql
+++ classwork/class1.sql
@@ -10,3 +10,5 @@ VALUES ('1','Alfred','21','computer science','67'),
 ('2','Kiran','12','maths','98'),
 ('3','Chong','23','physics','90'),
 ('4','Suresh','32','computer science','93');
+
+SELECT * FROM students WHERE age > 20;
