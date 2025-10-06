22f699894033d068ea9de51c691f47475961199b
Shahziyaa <shahziya2003@gmail.com>
2025-10-06 10:55:22 +0530

    home10.sql

---
+++ homework/home10.sql
@@ -0,0 +1,14 @@
+CREATE TABLE authors (
+    authorid INT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    UNIQUE (email)
+);
+
+CREATE TABLE books(
+    authorid INT,
+    bookid INT PRIMARY KEY,
+    title VARCHAR(100),
+    FOREIGN KEY (authorid) REFERENCES authors(authorid),
+    UNIQUE (bookid)
+)
